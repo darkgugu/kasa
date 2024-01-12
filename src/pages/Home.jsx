f13a8503f9d0e5a46f334fc7164f26841f9f8acb
Icham Duret <57659587+darkgugu@users.noreply.github.com>
2024-01-12 12:56:26 +0100

    Homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,25 @@
+import Banner from '../components/Banner'
+import Card from '../components/Card'
+import '../style/css/Home.css'
+import data from '../assets/logements.json'
+
+console.log(data)
+
 function Home() {
-	return <h1>Home</h1>
+	return (
+		<div className="Home">
+			<Banner />
+			<div className="card-container">
+				{data.map((logement) => (
+					<Card
+						key={logement.id}
+						title={logement.title}
+						picture={logement.pictures[0]}
+					/>
+				))}
+			</div>
+		</div>
+	)
 }
 
 export default Home
