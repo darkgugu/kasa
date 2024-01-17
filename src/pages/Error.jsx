2d7b6a16831a7e1c88962f199808c71c3e15ba4d
Icham Duret <57659587+darkgugu@users.noreply.github.com>
2024-01-17 17:11:39 +0100

    Page 404

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,5 +1,14 @@
+import { Link } from 'react-router-dom'
+import '../style/css/Error.css'
+
 function Error() {
-	return <h1>404</h1>
+	return (
+		<div className="Error">
+			<div>404</div>
+			<p>Oups! La page que vous demandez n'existe pas.</p>
+			<Link to="/">Retourner sur la page d'accueil</Link>
+		</div>
+	)
 }
 
 export default Error
