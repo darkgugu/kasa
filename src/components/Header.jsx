e3a0ffeaec57082ad338bb36bca807e188e67d95
Icham Duret <57659587+darkgugu@users.noreply.github.com>
2024-01-24 18:28:23 +0100

    Ajout underline

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,39 @@
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 import logo from '../assets/LOGO.png'
 import '../style/css/Header.css'
 
 function Header() {
+	const { pathname } = useLocation()
+
+	if (pathname === '/') {
+		//document.getElementById('accueil').className = 'underlined'
+	} else {
+		//document.getElementById('propos').className = 'underlined'
+	}
 	return (
 		<nav>
 			<img src={logo} alt="logo" />
 			<div>
-				<Link to="/">Acceuil</Link>
-				<Link to="/propos">A Propos</Link>
+				<Link
+					style={
+						pathname === '/'
+							? { textDecoration: 'underline' }
+							: null
+					}
+					to="/"
+				>
+					Acceuil
+				</Link>
+				<Link
+					style={
+						pathname === '/propos'
+							? { textDecoration: 'underline' }
+							: null
+					}
+					to="/propos"
+				>
+					A Propos
+				</Link>
 			</div>
 		</nav>
 	)
